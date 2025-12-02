8edd23ffb29880d004d8e18258d737cc3973dca3
David William dos Santos <ksgulosao@gmail.com>
2025-12-02 12:03:33 -0300

    Add responsive nav menu and theme switcher
    Introduced a hamburger menu for mobile navigation and a theme switcher button using Bootstrap Icons. Updated CSS for responsive navigation, button and hover effects, and added JavaScript to handle menu toggling and theme switching. Minor style improvements and refactoring for better user experience.

---
+++ dragon-quest/script.js
@@ -0,0 +1,28 @@
+const body = document.querySelector("body");
+const navBar = document.querySelector(".nav-bar");
+const menuIcon = document.querySelector(".menu-icon");
+const themeButton = document.querySelector(".theme-button");
+
+/* Menu Hamburger */
+function openMenu() {
+  navBar.classList.toggle("open-menu");
+  if (navBar.classList.contains("open-menu")) {
+    menuIcon.classList.remove("bi-list");
+    menuIcon.classList.add("bi-x");
+  } else {
+    menuIcon.classList.remove("bi-x");
+    menuIcon.classList.add("bi-list");
+  }
+}
+
+/* Switch Theme */
+function switchTheme() {
+  body.classList.toggle("dark-theme");
+  if (body.classList.contains("dark-theme")) {
+    themeButton.classList.remove("bi-moon-fill");
+    themeButton.classList.add("bi-brightness-high-fill");
+  } else {
+    themeButton.classList.remove("bi-brightness-high-fill");
+    themeButton.classList.add("bi-moon-fill");
+  }
+}
